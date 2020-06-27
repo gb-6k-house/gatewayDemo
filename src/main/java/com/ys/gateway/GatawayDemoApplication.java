f084edd1ac0b9ad9af191598b60c8d3636adb55b
David <csu_liukai_web@163.com>
2020-06-27 10:39:45 +0800

    inital

---
+++ src/main/java/com/ys/gateway/GatawayDemoApplication.java
@@ -0,0 +1,12 @@
+package com.ys.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatawayDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatawayDemoApplication.class, args);
+    }
+
+}
